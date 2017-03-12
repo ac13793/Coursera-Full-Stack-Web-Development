69046b3231fc8eca0f86d201b5db86d7b14850c0
Ankit Chaurasia <Ankit Chaurasia>
2017-03-12 13:35:59 +0530

    Course5 Week4 Module3 Exercise

--- 5. Server-side Development with NodeJS/Exercise/rest-server-passport/app.js
+++ 5. Server-side Development with NodeJS/Exercise/rest-server-passport/app.js
@@ -6,9 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
 var config = require('./config');
+var authenticate = require('./authenticate');
 
 mongoose.connect(config.mongoUrl);
 var db = mongoose.connection;
@@ -26,6 +25,16 @@ var leaderRouter = require('./routes/leaderRouter');
 
 var app = express();
 
+// Secure traffic only
+app.all('*', function(req, res, next){
+    console.log('req start: ',req.secure, req.hostname, req.url, app.get('port'));
+  if (req.secure) {
+    return next();
+  };
+
+ res.redirect('https://'+req.hostname+':'+app.get('secPort')+req.url);
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -38,11 +47,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // passport config
-var User = require('./models/user');
 app.use(passport.initialize());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- 5. Server-side Development with NodeJS/Exercise/rest-server-passport/config.js
+++ 5. Server-side Development with NodeJS/Exercise/rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '1173885782723794',
+        clientSecret: '4e6075bf7bbecdcc79a939868faf9c03',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
